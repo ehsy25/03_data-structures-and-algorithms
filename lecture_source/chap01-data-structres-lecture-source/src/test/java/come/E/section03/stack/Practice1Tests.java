c56195966865221130f7f6d27fc690b1ac432780
ehsy25 <eyoun17@gmail.com>
2025-11-05 18:22:55 +0900

    algo problems

---
+++ lecture_source/chap01-data-structres-lecture-source/src/test/java/come/E/section03/stack/Practice1Tests.java
@@ -0,0 +1,41 @@
+package come.E.section03.stack;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*괄호 백준 9012*/
+class Practice1Tests {
+
+    Practice1 p1;
+
+    @BeforeEach
+    void setUp() {
+        p1 = new Practice1();
+    }
+
+    static Stream<Arguments> provideTestCases(){
+        return Stream.of(
+          Arguments.arguments("(())())", "NO"),
+          Arguments.arguments("(((()())()", "NO"),
+          Arguments.arguments("(()())((()))", "YES"),
+          Arguments.arguments("((()()(()))(((())))()", "NO"),
+          Arguments.arguments("()()()()(()()())()", "YES"),
+          Arguments.arguments("(()((())()(", "NO")
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    void testeParenthesisString(String input, String expected){
+        String result = p1.solution(input);
+        Assertions.assertEquals(expected,result);
+    }
+
+}
