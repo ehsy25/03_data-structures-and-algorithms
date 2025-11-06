41095927dded62c75481676a6f3877cc7bcd5e75
macacolabs <monkey.ohgiraffers@gmail.com>
2025-11-06 15:07:57 +0900

    minheap commit

---
+++ chap01-data-structures/src/main/java/com/ohgiraffers/section07/heap/Application.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.section07.heap;
+
+import java.util.PriorityQueue;
+
+/* Java Collection Framework에서 heap을 직접 지원하는 클래스는 없지만
+* PriorityQueue클래스를 이용하여 heap을 구현할 수 있다.
+* */
+public class Application {
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    }
+}
