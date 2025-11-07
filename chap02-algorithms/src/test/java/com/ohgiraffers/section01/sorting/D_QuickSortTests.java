a1d1e14e29897b0ad22badea3be573ea280a934f
macacolabs <monkey.ohgiraffers@gmail.com>
2025-11-07 11:11:05 +0900

    QuickSorting test코드 업로드

--- chap02-algorithms/src/test/java/com/ohgiraffers/section01/sorting/D_QuickSortTests.java
+++ chap02-algorithms/src/test/java/com/ohgiraffers/section01/sorting/D_QuickSortTests.java
@@ -2,7 +2,6 @@ package com.ohgiraffers.section01.sorting;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,31 +10,27 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-class C_InsertionSortTests {
+class D_QuickSortTests {
 
-    static int[] act1, act2, exp1, exp2;
+    static int[] act, exp;
 
     @BeforeAll
     static void setUp() {
-        act1 = new int[] {34, 23, 5, 24, 1, 9, 12};
-        exp1 = new int[] {1, 5, 9, 12, 23, 24, 34};
-        act2 = new int[] {40, 47, 38, 8, 33, 35};
-        exp2 = new int[] {8, 33, 35, 38, 40, 47};
+        act = new int[] {5, 3, 8, 9, 2, 4, 7};
+        exp = new int[] {2, 3, 4, 5, 7, 8, 9};
     }
 
     static Stream<Arguments> provideAscendingSource() {
         return Stream.of(
-                Arguments.of(act1, exp1),
-                Arguments.of(act2, exp2)
+                Arguments.of(act, exp)
         );
     }
 
-    @DisplayName("삽입 정렬 테스트")
+    @DisplayName("퀵 정렬 테스트")
     @ParameterizedTest
     @MethodSource("provideAscendingSource")
-    void testInsertionSort(int[] actual, int[] expected) {
-        C_InsertionSort.solution(actual);
+    void testQuickSort(int[] actual, int[] expected) {
+        D_QuickSort.solution(actual);
         Assertions.assertArrayEquals(expected, actual);
-        System.out.println(Arrays.toString(actual));
     }
 }
