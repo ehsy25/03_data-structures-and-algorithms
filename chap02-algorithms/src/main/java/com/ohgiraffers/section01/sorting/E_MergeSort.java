3b9a9489f6d55da687bd21fd8840ac3cecafc65b
macacolabs <monkey.ohgiraffers@gmail.com>
2025-11-07 11:39:29 +0900

    MergeSorting test코드 업로드

---
+++ chap02-algorithms/src/main/java/com/ohgiraffers/section01/sorting/E_MergeSort.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section01.sorting;
+
+/* 병합 정렬
+ * 분할 정복(Divide and Conquer) 방식으로 배열을 반씩 나누어 정렬한 후 병합(Merge)하는 방식이다.
+ * 정렬 된 두 개의 배열을 합치는 과정에서 안정 정렬(Stable Sort)의 특성을 유지할 수 있다.
+ * 시간 복잡도는 항상 O(n log n)으로 일정하며, 최악의 경우에도 O(n log n)이다.
+ * 하지만 추가 메모리 공간이 필요하므로 메모리 제한적 환경에서는 비효율적일 수 있다.
+ * */
+public class E_MergeSort {
+
+    /* 문제 : n 개의 정수가 주어졌을 때 병합 정렬 알고리즘을 사용하여 오름차순으로 정렬하는 프로그램 작성 */
+
+    public static void solution(int[] arr){
+
+    }
+}
