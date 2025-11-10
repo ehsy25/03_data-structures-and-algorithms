6f410a39113611959ca33c44e3f8a9eaee5ee274
macacolabs <monkey.ohgiraffers@gmail.com>
2025-11-10 09:26:35 +0900

    commit

--- chap02-algorithms/src/main/java/com/ohgiraffers/section02/searching/C_ShortestPath.java
+++ chap02-algorithms/src/main/java/com/ohgiraffers/section02/searching/C_ShortestPath.java
@@ -1,7 +1,7 @@
 package com.ohgiraffers.section02.searching;
 /*
 * BFS는 먼저 방문한 노드를 기준으로 가장 가까운 노드부터 탐색한다.
-* 즉, 시작점에서 출발하여 거리 1ㅉ라ㅣ 노드를 모두 방문한 후,
+* 즉, 시작점에서 출발하여 거리 1짜리 노드를 모두 방문한 후,
 * 거리 2짜리 노드를 방문하는 방식으로 진행된다.
 * 따라서 어떤 노드에 도달했을 때, 처음 도달한 순간이 곧 최단 거리가 된다.
 * DFS는 한 방향으로 깊게 들어간 후, 막다른 길에 도달하면 다시 돌아오므로
@@ -9,5 +9,19 @@ package com.ohgiraffers.section02.searching;
 * */
 public class C_ShortestPath {
 
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visit;
+    static int[] driX = {0, 0, -1, 1};
+    static int[] driY = {-1, 1, 0, 0};
 
+    static class Node{
+        int x;
+        int y;
+
+        public Node(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
