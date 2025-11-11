6c1b6d83fa1b2b8262c582964eecddfd147d7568
macacolabs <monkey.ohgiraffers@gmail.com>
2025-11-11 14:21:27 +0900

    commit

---
+++ chap02-algorithms/src/main/java/com/ohgiraffers/section04/dp/F_StairClimbing.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section04.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+public class F_StairClimbing {
+
+    public static int solution(String input) throws IOException {
+        BufferedReader br = new BufferedReader(new StringReader(input));
+        int n = Integer.parseInt(br.readLine());        // 계단의 개수
+
+
+        return 0;
+    }
+}
